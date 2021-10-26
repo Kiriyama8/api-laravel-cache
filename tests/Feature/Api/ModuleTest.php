a386ab518fd7b4198eba71eae048d70a3a5f7c3b
Vinicius Kiriyama <minumex8@gmail.com>
2021-10-26 15:12:53 +0000

    Add ModuleTest

---
+++ tests/Feature/Api/ModuleTest.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Tests\Feature\Api;
+
+use App\Models\Course;
+use App\Models\Module;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class ModuleTest extends TestCase
+{
+    /**
+     * A basic feature test example.
+     *
+     * @return void
+     */
+    public function test_get_all_modules_by_course()
+    {
+        $course = Course::factory()->create();
+
+        Module::factory()->count(10)->create([
+            'course_id' => $course->id
+        ]);
+
+        $response = $this->get("/courses/{$course->uuid}/modules");
+
+        $response->assertStatus(200)
+            ->assertJsonCount(10, 'data');
+    }
+}
