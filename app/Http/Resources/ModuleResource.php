5f37fe2914d280d6f61e33294efac22e0b7842bc
Kiriyama8 <minumex8@gmail.com>
2021-10-26 21:39:37 -0300

    Fix order resource

--- app/Http/Resources/ModuleResource.php
+++ app/Http/Resources/ModuleResource.php
@@ -2,7 +2,6 @@
 
 namespace App\Http\Resources;
 
-use Carbon\Carbon;
 use Illuminate\Http\Resources\Json\JsonResource;
 
 class ModuleResource extends JsonResource
@@ -16,8 +15,8 @@ class ModuleResource extends JsonResource
     public function toArray($request)
     {
         return [
-            'nome' => $this->name,
             'id' => $this->uuid,
+            'name' => $this->name,
             'lessons' => LessonResource::collection($this->whenLoaded('lessons'))
         ];
     }
